4161d1a3efeac13439002cbb57b8433b597b276e
No One <No.Ones.Email@gmail.com>
2016-03-09 16:55:59 -0800

    More more stuff

---
+++ src/RandomStuff.java
@@ -0,0 +1,12 @@
+
+public class RandomStuff {
+	public static void main(String[] args) {
+		int b = 1;
+		for (int i1 = 0; i1 < 3; i1++) {
+			System.out.println();
+			for (int i = 0; i < 3; i++) {
+				System.out.print(b++ );
+			}
+		}
+	}
+}
